b856b8c4f8c3cadd480feb36041e22405950b413
Stef Velzel <stefvelzel@hotmail.com>
2021-01-25 01:32:03 +0100

    Made test.h more cute and added documentation

--- tests/test.h
+++ tests/test.h
@@ -4,6 +4,36 @@
  *
  * groufix : graphics engine produced by Stef Velzel.
  * www     : <www.vuzzel.nl>
+ *
+ * This file contains the header-only testing utility.
+ * Use the following macros to describe and run a test:
+ *
+ * TEST_DESCRIBE(name, base)
+ *   Describe a new test, the syntax is similar to a function:
+ *   TEST_DESCRIBE(basic_test, t) { gfx_window_set_title(t->window, "test"); }
+ *   @name The name of the test.
+ *   @base The name of the exposed GFXTestBase pointer.
+ *
+ * TEST_FAIL()
+ *   Forces the test to fail and exits the program.
+ *
+ * TEST_RUN(name)
+ *   Call from within a test, run another test by name.
+ *
+ * TEST_MAIN(name)
+ *   Main entry point of the program by test name, use as follows:
+ *   TEST_MAIN(basic_test);
+ *
+ * The testing utility initializes groufix and opens a window backed by a
+ * default renderer setup. To override default behaviour you can disable some
+ * building steps, define one of the following before includng this file:
+ *
+ * TEST_SKIP_EVENT_HANDLERS
+ *   Do not register the default event handlers for the base window.
+ *
+ * TEST_SKIP_CREATE_RENDER_GRAPH
+ *   Do not build a render graph,
+ *   i.e. no passes are added to the base renderer.
  */
 
 
@@ -13,19 +43,6 @@
 #include <groufix.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-
-/**
- * Base testing state, modify at your leisure :)
- */
-static struct GFXTestBase
-{
-	int initialized;
-
-	GFXWindow*   window;
-	GFXRenderer* renderer; // Window is attached at index 0.
-
-} _test_base = { 0, NULL, NULL };
 
 
 // Describes a test function that can be called.
@@ -48,6 +65,17 @@ static struct GFXTestBase
 		_test_end(); \
 	} \
 	int _test_unused_for_semicolon
+
+
+/**
+ * Base testing state, modify at your leisure :)
+ */
+static struct GFXTestBase
+{
+	GFXWindow*   window;
+	GFXRenderer* renderer; // Window is attached at index 0.
+
+} _test_base = { NULL, NULL };
 
 
 /**
@@ -109,7 +137,7 @@ static void _test_fail(void)
 }
 
 /**
- * End the test (which terminates groufix).
+ * End (i.e. exit) the test program.
  */
 static void _test_end(void)
 {
@@ -120,13 +148,10 @@ static void _test_end(void)
 }
 
 /**
- * Initializes the test base program (which initializes groufix).
+ * Initializes the test base program.
  */
 static void _test_init(void)
 {
-	if (_test_base.initialized)
-		return;
-
 	// Initialize.
 	if (!gfx_init())
 		_test_fail();
@@ -139,10 +164,12 @@ static void _test_init(void)
 	if (_test_base.window == NULL)
 		_test_fail();
 
+#if !defined (TEST_SKIP_EVENT_HANDLERS)
 	// Register the default key release event.
 	_test_base.window->events.key.release = _test_key_release;
+#endif
 
-	// Create a renderer and attach the window to index 0.
+	// Create a renderer and attach the window at index 0.
 	_test_base.renderer = gfx_create_renderer(NULL);
 	if (_test_base.renderer == NULL)
 		_test_fail();
@@ -154,14 +181,11 @@ static void _test_init(void)
 	// Create a single render pass that writes to the window.
 	GFXRenderPass* pass = gfx_renderer_add(_test_base.renderer, 0, NULL);
 	if (pass == NULL)
-		TEST_FAIL();
+		_test_fail();
 
 	if (!gfx_render_pass_write(pass, 0))
-		TEST_FAIL();
+		_test_fail();
 #endif
-
-	// Set to initialized and return.
-	_test_base.initialized = 1;
 }
 
 
